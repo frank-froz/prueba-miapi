f221c2b514709940de4fd96378f73a464cdc949e
frank-froz <franklin.huaytalla@tecsup.edu.pe>
2025-05-07 01:44:55 -0500

    update middleware

--- mi_api/mi_api/settings.py
+++ mi_api/mi_api/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'https://prueba-miapi.onrender.com,
+    'https://prueba-miapi.onrender.com',
 ]
 
 ROOT_URLCONF = 'mi_api.urls'
